a891c7a851e3bf10be9733b1631d0bd6a28b00ae
youmin1017 <xx4016481@gmail.com>
2022-10-24 15:05:21 +0800

    update [LunarVim/nvim-basic-ide](https://github.com/LunarVim/nvim-basic-ide) configurations

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -57,11 +57,29 @@ nvim_tree.setup {
     width = 30,
     side = "left",
     mappings = {
+      -- custom only false will merge the list with the default mappings
+      -- if true, it will only use your list to set the mappings
+      custom_only = true,
+      -- list of mappings to set on the tree manually
       list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
+        { key = {'<CR>', 'l', 'o'}, cb = tree_cb('edit') },
+        { key = 'i', cb = tree_cb('split')},
+        { key = 's', cb = tree_cb('vsplit')},
+        { key = 'r', cb = tree_cb('refresh')},
+        { key = 'yy', cb = tree_cb('copy_name')},
+        { key = 'gy', cb = tree_cb('copy_path') },
+        { key = 'gY', cb = tree_cb('copy_absolute_path') },
+        { key = '^', cb = tree_cb('dir_up') },
+        { key = '?', cb = tree_cb('toggle_help')},
+        { key = 't', cb = tree_cb('tabnew') },
+        { key = '<F2>', cb = tree_cb('rename') },
+        { key = '<C-x>', cb = tree_cb('cut') },
+        { key = '<C-c>', cb = tree_cb('copy')},
+        { key = '<C-v>', cb = tree_cb('paste') },
+        { key = 'C', cb = tree_cb('create') },
+        { key = 'D', cb = tree_cb('remove') },
+        { key = '<C-d>', cb = tree_cb("cd") },
+      }
+    }
   },
 }
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,5 +1,5 @@
 vim.opt.backup = false                          -- creates a backup file
-vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
+-- vim.opt.clipboard = "unnamedplus"               -- allows neovim to access the system clipboard
 vim.opt.cmdheight = 1                           -- more space in the neovim command line for displaying messages
 vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
 vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
@@ -16,7 +16,7 @@ vim.opt.splitbelow = true                       -- force all horizontal splits t
 vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
 vim.opt.swapfile = false                        -- creates a swapfile
 vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
-vim.opt.timeoutlen = 1000                       -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.timeoutlen = 100                       -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.undofile = true                         -- enable persistent undo
 vim.opt.updatetime = 300                        -- faster completion (4000ms default)
 vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
